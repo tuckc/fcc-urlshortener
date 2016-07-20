a3eba8ca341b79be288138d189d8451080943bfa
Colton <coltontuck94@gmail.com>
2016-07-20 14:33:40 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  const ip = req.headers['x-forwarded-for'];
+  const lang = req.headers["accept-language"].split(',')[0];
+  var os = (/.+(\(.+\)).+/gi).exec(req.headers['user-agent'])[1];
+  os = os.replace('(', '');
+  os = os.replace(')', '');
+  
+  const whoami = {
+    ipaddress: ip,
+    language: lang,
+    software: os
+  };
+  
+  res.json(whoami);
+});
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log('Example app listening on port ' + process.env.PORT || 8080);
+});
+
